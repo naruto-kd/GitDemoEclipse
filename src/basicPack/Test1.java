038971598f51ebed22ca2177be090656786e9ca2
Dinesh GitHub <dinedinesh2001@gmail.com>
2022-03-08 15:34:19 +0530

    This is my first commit as a lead. This is the project initiation as well.

---
+++ src/basicPack/Test1.java
@@ -0,0 +1,13 @@
+package basicPack;
+
+public class Test1 {
+	public static void main(String args[]) {
+		String hungry = "yes";
+		if(hungry=="yes") {
+			System.out.println("Eat first! ");
+		}
+		else {
+			System.out.println("okay");
+		}
+	}
+}
